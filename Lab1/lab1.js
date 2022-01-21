c666d2a4b755b7fc41557483666b9a611517bb08
bemin.shaker <beminshaker1@outlook.com>
2022-01-20 23:24:29 -0500

    hello

---
+++ Lab1/lab1.js
@@ -0,0 +1,46 @@
+// I pledge my honor that I have abided by the Stevens Honor System. - Bemin Shaker
+const questionOne = function questionOne(arr) {
+  let total = 0;
+  arr.forEach((value) => {
+    total += value * value;
+  });
+  return total;
+};
+
+const questionTwo = function questionTwo(num) {
+  // Implement question 2 here
+};
+
+const questionThree = function questionThree(text) {
+  let counter = 0;
+  for (let char in text) {
+    if (
+      text[char] == "a" ||
+      text[char] == "e" ||
+      text[char] == "i" ||
+      text[char] == "o" ||
+      text[char] == "u"
+    ) {
+      counter++;
+    }
+  }
+  return counter;
+};
+
+const questionFour = function questionFour(num) {
+  let x = 1;
+
+  for (const i = num; i <= 1; i--) {
+    x = x * i;
+  }
+};
+
+module.exports = {
+  firstName: "Bemin",
+  lastName: "Shaker",
+  studentId: "10445235",
+  questionOne,
+  questionTwo,
+  questionThree,
+  questionFour,
+};
