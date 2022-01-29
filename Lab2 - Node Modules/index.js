9e913126ba9618ba5b8104681310ccf50abe8358
bemin.shaker <beminshaker1@outlook.com>
2022-01-29 17:55:57 -0500

    Lab 2 progress

--- Lab2 - Node Modules/index.js
+++ Lab2 - Node Modules/index.js
@@ -1,6 +1,9 @@
 const arrayUtils = require("./arrayUtils");
+const stringUtils = require("./stringUtils");
+const objUtils = require("./objUtils");
+
 //console.log(arrayUtils.mean([1, 2, 3]));
-console.log(arrayUtils.medianSquared([1, 2, 3, 4, 5]));
+console.log(stringUtils.camelCase(123));
 
 // Mean Tests
 try {
@@ -19,10 +22,27 @@ try {
   console.log("mean failed successfully");
 }
 
+// Median Squared Tests
+try {
+  // Should Pass
+  const medianOne = medianSquared([8, 3, 4, 5]);
+  console.log("mean passed successfully");
+} catch (e) {
+  console.error("mean failed test case");
+}
+
+try {
+  // Should Fail
+  const medianTwo = medianSquared("This is string");
+  console.error("mean did not error");
+} catch (e) {
+  console.log("mean failed successfully");
+}
+
 // Fill Tests
 try {
   // Should Pass
-  const fillOne = fill(-6);
+  const fillOne = fill(6);
   console.log("fill passed successfully");
 } catch (e) {
   console.error("fill failed test case");
---
+++ Lab2 - Node Modules/stringUtils.js
@@ -0,0 +1,41 @@
+//Checks if string exists, the length of the string is greater than 0, and if the string is of proper type
+function checkString(string) {
+  if (!string) {
+    throw "Error: Input not supplied or undefined";
+  }
+  if (string.length <= 0) {
+    throw "Error: Input must not be an empty string";
+  }
+  if (typeof string != "string") {
+    throw "Error: Input must an string type";
+  }
+}
+
+//Returns a camel case version of the input string
+function camelCase(string) {
+  checkString(string);
+  let newString = "";
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == " ") {
+      newString += string[i + 1].toUpperCase();
+      i++;
+    } else {
+      if (i == 0) {
+        newString += string[i].toLowerCase();
+        i++;
+      }
+      newString += string[i].toLowerCase();
+    }
+  }
+
+  return newString;
+}
+
+function replaceChar(string) {}
+
+function mashUp(string1, string2) {}
+module.exports = {
+  camelCase,
+  replaceChar,
+  mashUp,
+};
