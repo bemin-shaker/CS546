40cb80069538b4b632f80d3f140557c670b661e3
bemin.shaker <beminshaker1@outlook.com>
2022-02-11 20:58:27 -0500

    Lab4

---
+++ Lab4 - MongoDB/data/bands.js
@@ -0,0 +1,24 @@
+async function create(
+  name,
+  genre,
+  website,
+  recordLabel,
+  bandMembers,
+  yearFormed
+) {}
+
+async function getAll() {}
+
+async function get(id) {}
+
+async function remove(id) {}
+
+async function rename(id, newName) {}
+
+module.exports = {
+  create,
+  getAll,
+  get,
+  remove,
+  rename,
+};
