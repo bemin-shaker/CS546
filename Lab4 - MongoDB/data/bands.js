1d5117b12d3e073bd04f93fd22e3363912eb2dde
bemin.shaker <beminshaker1@outlook.com>
2022-02-14 19:04:58 -0500

    Lab4

--- Lab4 - MongoDB/data/bands.js
+++ Lab4 - MongoDB/data/bands.js
@@ -1,3 +1,111 @@
+const mongoCollections = require("../config/mongoCollections");
+const bands = mongoCollections.bands;
+const { ObjectId } = require("mongodb");
+
+function checkParam(
+  name,
+  genre,
+  website,
+  recordLabel,
+  bandMembers,
+  yearFormed
+) {
+  if (
+    !name ||
+    !genre ||
+    !website ||
+    !recordLabel ||
+    !bandMembers ||
+    !yearFormed
+  ) {
+    throw "Error: All data fields must be provided";
+  }
+
+  if (
+    typeof name !== "string" ||
+    typeof website !== "string" ||
+    typeof recordLabel !== "string"
+  ) {
+    throw "Error: name, website, and recordLabal must be strings";
+  }
+
+  if (
+    name.trim().length == 0 ||
+    website.trim().length == 0 ||
+    recordLabel.trim().length == 0
+  ) {
+    throw "Error: name, website, and recordLabal must not be empty strings";
+  }
+
+  if (!Array.isArray(genre) || !Array.isArray(bandMembers)) {
+    throw "Error: genre and bandMembers must be arrays";
+  }
+
+  let invalidElement1 = false;
+  for (i in genre) {
+    if (typeof genre[i] !== "string" || genre[i].trim().length === 0) {
+      invalidElement1 = true;
+      break;
+    }
+    genre[i] = genre[i].trim();
+  }
+
+  let invalidElement2 = false;
+  for (i in bandMembers) {
+    if (
+      typeof bandMembers[i] !== "string" ||
+      bandMembers[i].trim().length === 0
+    ) {
+      invalidElement2 = true;
+      break;
+    }
+    bandMembers[i] = bandMembers[i].trim();
+  }
+
+  if (invalidElement1 || invalidElement2) {
+    throw "Error: genre and/or bandMembers contain invalid elements or empty strings";
+  }
+
+  if (typeof yearFormed != "number" || yearFormed < 1900 || yearFormed > 2022) {
+    throw "Error: yearFormed must be a valid number between 1900 and 2022";
+  }
+
+  if (
+    !website.includes("http://www.") ||
+    !website.includes(".com") ||
+    website.length < 20
+  ) {
+    throw "Error: website must contain 'http://www.' and end in a '.com', and have at least 5 characters in-between the 'http://www.' and '.com'";
+  }
+}
+
+function checkIdParam(id) {
+  if (!id) throw "Error: You must provide an id to search for";
+  if (typeof id !== "string") throw "Error: Id must be a string";
+  if (id.trim().length === 0)
+    throw "Error: Id cannot be an empty string or just spaces";
+  id = id.trim();
+  if (!ObjectId.isValid(id)) throw "Error: invalid object ID";
+}
+
+function checkName(name) {
+  if (!name) throw "Error: You must provide a name";
+  if (typeof name !== "string") throw "Error: Name must be a string";
+  if (name.trim().length === 0)
+    throw "Error: Name cannot be an empty string or just spaces";
+}
+
+async function get(id) {
+  checkIdParam(id);
+  const bandCollection = await bands();
+  const band = await bandCollection.findOne({
+    _id: ObjectId(id),
+  });
+  if (band === null) throw "Error: No band with that id";
+  band["_id"] = band["_id"].toString();
+  return band;
+}
+
 async function create(
   name,
   genre,
@@ -5,15 +113,73 @@ async function create(
   recordLabel,
   bandMembers,
   yearFormed
-) {}
+) {
+  checkParam(name, genre, website, recordLabel, bandMembers, yearFormed);
+  name = name.trim();
+  const bandCollection = await bands();
 
-async function getAll() {}
+  let newBand = {
+    name: name,
+    genre: genre,
+    website: website,
+    recordLabel: recordLabel,
+    bandMembers: bandMembers,
+    yearFormed: yearFormed,
+  };
 
-async function get(id) {}
+  const insertInfo = await bandCollection.insertOne(newBand);
 
-async function remove(id) {}
+  if (!insertInfo.acknowledged || !insertInfo.insertedId)
+    throw "Error: Could not add band";
 
-async function rename(id, newName) {}
+  const newId = insertInfo.insertedId.toString();
+  const band = await this.get(newId);
+  return band;
+}
+
+async function getAll() {
+  const bandCollection = await bands();
+  const bandList = await bandCollection.find({}).toArray();
+  if (!bandList) throw "Could not get all bands";
+  return bandList;
+}
+
+async function remove(id) {
+  checkIdParam(id);
+  const bandCollection = await bands();
+  const band = await this.get(id);
+  let bandName = band["name"];
+  const deletionInfo = await bandCollection.deleteOne({ _id: ObjectId(id) });
+  if (deletionInfo.deletedCount === 0) {
+    throw `Could not delete dog with id of ${id}`;
+  }
+  return `${bandName} has been successfully deleted!`;
+}
+
+async function rename(id, newName) {
+  checkIdParam(id);
+  checkName(newName);
+
+  const bandCollection = await bands();
+  const band = await this.get(id);
+  if (newName === band["name"]) {
+    throw "Error: newName is the same as the current value stored in the database";
+  }
+  const updatedBand = {
+    name: newName,
+  };
+
+  const updatedInfo = await bandCollection.updateOne(
+    { _id: ObjectId(id) },
+    { $set: updatedBand },
+    { upsert: true }
+  );
+  if (updatedInfo.modifiedCount === 0) {
+    throw "could not update band successfully";
+  }
+
+  return await this.get(id);
+}
 
 module.exports = {
   create,
--- Lab4 - MongoDB/index.js
+++ Lab4 - MongoDB/index.js
@@ -1,1 +1,92 @@
+//I pledge my honor that I have abided by the Stevens Honor System. - Bemin Shaker
 const bands = require("./data/bands");
+
+async function main() {
+  const queen = await bands.create(
+    "Queen",
+    ["Progressive Rock", "Art rock"],
+    "http://www.queenonline.com",
+    "EMI",
+    ["Freddie Mercury", "Brian May", "John Deacon", "Roger Taylor"],
+    1970
+  );
+  console.log(queen);
+  const foo = await bands.create(
+    "Foo Fighters",
+    ["Alternative Rock", "Punk rock"],
+    "http://www.foofighters.com",
+    "RCA",
+    [
+      "Dave Grohl",
+      "Taylor Hawkins",
+      "Pat Smear",
+      "Chris Shiflett",
+      "Nate Mendel",
+      "Rami Jaffee",
+      "Franz Stahl",
+      "William Goldsmith",
+    ],
+    1994
+  );
+  console.log(foo);
+  console.log(await bands.getAll());
+  const helloBand = await bands.create(
+    "Hello",
+    ["Alternative Rock", "Art rock"],
+    "http://www.helloband.com",
+    "ASE",
+    ["John Smith", "Brian Rogers", "Sam Samuel", "Roger Cambridge"],
+    1988
+  );
+  console.log(helloBand);
+  const renamedQueen = await bands.rename(
+    "620aec7cd02669d00c67739a",
+    "Lennon Boys"
+  );
+  console.log(renamedQueen);
+  const removeFoo = await bands.remove("620aec7cd02669d00c67739b");
+  console.log(removeFoo);
+  console.log(await bands.getAll());
+  try {
+    const createInvalid = await bands.create(
+      "Queen",
+      "Progressive Rock",
+      "http://www.queenonline.com",
+      "EMI",
+      ["Freddie Mercury", "Brian May", "John Deacon", "Roger Taylor"],
+      1970
+    );
+    console.log(createInvalid);
+  } catch (e) {
+    console.log(e);
+  }
+  try {
+    const removeInvalid = await bands.remove("620ae74960313fd4e9cabb0fs");
+    console.log(removeInvalid);
+  } catch (e) {
+    console.log(e);
+  }
+  try {
+    const renameInvalid = await bands.rename(
+      "620ae74960313fd4e9cabb0s",
+      "Queen"
+    );
+    console.log(renameInvalid);
+  } catch (e) {
+    console.log(e);
+  }
+  try {
+    const renameInvalid2 = await bands.rename("620ae74960313fd4e9cabb0f", 1344);
+    console.log(renameInvalid2);
+  } catch (e) {
+    console.log(e);
+  }
+  try {
+    const getInvalid = await bands.get("620ae74960313fd4e9cabb0fs");
+    console.log(getInvalid);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+main();
